a100cdb8713dc2206c9f4574ce7f7dfde19c00b5
Kashmira9890 <kashmiranagwekar@gmail.com>
2017-08-04 13:17:19 +0530

    Updated version.php

--- version.php
+++ version.php
@@ -29,5 +29,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version    = 2017052800;   // The current module version (Date: YYYYMMDDXX)
 $plugin->requires   = 2017042100;   // Requires this Moodle version
 $plugin->component  = 'report_mergefiles'; // Full name of the plugin (used for diagnostics)
-$plugin->maturity   = MATURITY_BETA;
+$plugin->maturity   = MATURITY_ALPHA;
 $plugin->release    = 'v3.3-r1';    // This is our first revision for Moodle 3.3 branch.
